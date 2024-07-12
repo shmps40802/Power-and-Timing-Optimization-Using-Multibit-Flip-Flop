081235dc19ff2dc789599f52d01283e2c5d030be
Perry9124 <124435956+Perry9124@users.noreply.github.com>
2024-07-12 20:06:51 +0800

    Update Board.h

--- Board.h
+++ Board.h
@@ -5,12 +5,10 @@
 #include <list>
 #include <set>
 #include <map>
-#include <unordered_set>
 #include <unordered_map>
 #include "Cell.h"
 #include "FlipFlop.h"
 #include "Gate.h"
-
 struct net{
 	string name;
 	set<string> points;
@@ -21,10 +19,10 @@ struct net{
 };
 struct row{
 	int col;
-	Cell* c;
-	row(int col, Cell* c){
+	string name;
+	row(int col, string name){
 		this->col = col;
-		this->c = c;
+		this->name = name;
 	}
 };
 class Board{
@@ -37,8 +35,6 @@ private:
 	int HigherRightX;
 	int LowerLeftY;
 	int HigherRightY;
-	int NumInput;
-	int NumOutput;
 	int NumInst;
 	int BinWidth;
 	int BinHeight;
@@ -49,23 +45,28 @@ private:
 	int SiteHeight;
 	int TotalNumOfSites;
 	float DisplacementDelay;
-	vector<Point> Input;                            // input pin(s)
-	vector<Point> Output;                           // output pin(s)
-	map<string, FlipFlop> FlipFlopLib;              // name to FlipFlop
-	map<string, Gate> GateLib;                      // name to Gate
-	map<string, FlipFlop*> InstFlipFlop;            // inst name to FlipFlop
-	map<string, Gate*> InstGate;                    // inst name to Gate
-	vector<net> Net;                                // net connection
-	map<int, list<row>> Location;                   // location of FlipFlop
-	map<pair<int,int>, vector<int>> PlacementRows;  // grid point info
+	int CellNumber;
+	vector<Point> Input;                             // input pin(s)
+	vector<Point> Output;                            // output pin(s)
+	map<string, FlipFlop> FlipFlopLib;               // name to FlipFlop
+	map<string, Gate> GateLib;                       // name to Gate
+	map<string, FlipFlop> InstToFlipFlop;            // inst name to FlipFlop
+	map<string, Gate> InstToGate;                    // inst name to Gate
+	vector<net> Net;                                 // net connection
+	map<int, list<row>> Location;                    // location of FlipFlop
+	map<pair<int, int>, vector<int>> PlacementRows;  // grid point info
+	set<string> NewFlipFlop;                         // initial FlipFlop
+	map<string, string> PrevToCur;                   // 
+	map<string, string> CurToPrev;                   //
 public:
     Board();
 	~Board();
     void ReadFile(); // read file
 	void Display();
 	Point NametoPoint(string);
-	vector<FlipFlop> Banking(vector<FlipFlop>);
-	vector<FlipFlop> Debanking(FlipFlop);
+	vector<FlipFlop> Banking(vector<FlipFlop>);  // only banking 1 bit
+	vector<FlipFlop> Debanking(FlipFlop);        // only debanking into 1 bit
+	FlipFlop getFlipFlop(int, int, int);         //
 	bool Check(int, int);
 	int ManhattanDist(Point, Point);
 	int TNSCost();
