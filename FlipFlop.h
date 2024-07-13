96db58c630a05c3d9cc109069d802c6c14aa785c
Perry9124 <124435956+Perry9124@users.noreply.github.com>
2024-07-13 21:14:50 +0800

    Update FlipFlop.h

--- FlipFlop.h
+++ FlipFlop.h
@@ -1,22 +1,24 @@
 #ifndef FLIPFLOP_H
 #define FLIPFLOP_H
 #include "Cell.h"
+#include <map>
 using namespace std;
 class FlipFlop : public Cell {
 private:
-	int N;             // n bit(s)
-	float QpinDelay;   // delay time of Qpin
-	float GatePower;   // power of flipflop
+	int N;                     // n bit(s)
+	float QpinDelay;           // delay time of Qpin
+	float GatePower;           // power of flipflop
+	map<string, float> slack;  // slack of flipflop
 public:
 	FlipFlop();
-	FlipFlop(int, int, int, int, vector<Point>);  // N width height D Q P
-	int getN(void);
-	void setSlack(string, float);
-	float getSlack(string);
+	FlipFlop(int, int, int, int, vector<Point>);  // N width height P Pin
+	int getN(void);                               // get bit number
 	void setPower(float);
 	float getPower(void);
 	void setQpinDelay(float);
 	int getQpinDelay(void);
 	void display(void);
+    void setSlack(string, float);
+    float getSlack(string);
 };
 #endif
