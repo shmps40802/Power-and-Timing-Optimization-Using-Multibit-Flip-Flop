5c2663a249f89d49cb598633f868cdc8a4a83427
Perry9124 <124435956+Perry9124@users.noreply.github.com>
2024-07-20 20:57:58 +0800

    Update Cell.cpp

--- Cell.cpp
+++ Cell.cpp
@@ -1,6 +1,6 @@
 #include "Cell.h"
 #include <iostream>
-Cell::Cell(){
+Cell::Cell() {
     Width = 0;
     Height = 0;
     PinCounts = 0;
@@ -14,46 +14,53 @@ void Cell::setPos(int x, int y) {
     this->x = x;
     this->y = y;
 }
-int Cell::getX(void){
+int Cell::getX(void) {
     return x;
 }
-int Cell::getY(void){
+int Cell::getRight(void) {
+    return x + Width;
+}
+int Cell::getY(void) {
     return y;
+}
+int Cell::getTop(void) {
+    return y + Height;
 }
 void Cell::setPin(vector<Point> pin) {
     Pin = pin;
 }
 vector<Point> Cell::getPin(void) {
+    if(Pin.empty())cout << "Pin in Cell "<< InstName << " is empty\n";
     return Pin;
 }
-int Cell::getWidth(void){
+int Cell::getWidth(void) {
     return Width;
 }
-int Cell::getHeight(void){
+int Cell::getHeight(void) {
     return Height;
 }
-int Cell::getArea(void){
+int Cell::getArea(void) {
     return Width * Height;
 }
-void Cell::setInstName(string name){
+void Cell::setInstName(string name) {
     InstName = name;
 }
-string Cell::getInstName(void){
+string Cell::getInstName(void) {
     return InstName;
 }
-void Cell::setCellName(string name){
+void Cell::setCellName(string name) {
     CellName = name;
 }
-string Cell::getCellName(void){
+string Cell::getCellName(void) {
     return CellName;
 }
-int Cell::getPinCount(void){
+int Cell::getPinCount(void) {
     return PinCounts;
 }
-Point Cell::getPoint(string name){
-    for(auto &it : Pin) {
+Point Cell::getPoint(string name) {
+    for (auto& it : Pin) {
         if(it.name == name)return it;
     }
-    std::cout << "Point name " << name << " not be found\n";
+    std::cout << "Point name " << name << " in " << CellName << " not be found\n";
     return Point(-1, -1, "");
 }
