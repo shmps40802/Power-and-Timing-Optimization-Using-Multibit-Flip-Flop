33c521eea0b9ed632acc1b1c0a4a0264e5bc0413
C. Y. Tseng <118676150+shmps40802@users.noreply.github.com>
2024-07-20 19:52:25 +0800

    Update Board.cpp
    Cost() variable sum -> float

--- Board.cpp
+++ Board.cpp
@@ -865,7 +865,7 @@ float Board::BinCost() {
 	return sum;
 }
 float Board::Cost() {
-	int sum = 0;
+	float sum = 0;
 	sum += Alpha * TNSCost() + Beta * PowerCost() + Gemma * AreaCost() + Delta * BinCost();
 	return sum;
 }
