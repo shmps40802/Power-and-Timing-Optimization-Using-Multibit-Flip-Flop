7ed8f92518e13fd4cf563e3b00ea71e252729fa4
Perry9124 <124435956+Perry9124@users.noreply.github.com>
2024-07-20 20:59:04 +0800

    Update Cell.h

--- Cell.h
+++ Cell.h
@@ -5,7 +5,7 @@
 using namespace std;
 struct Point {
 	string name;     // name of point
-	char type;       // D, Q, Clk, I, O
+    char type;       // D, Q, Clk, I, O
 	int x;           // x coordinate of point
 	int y;           // y coordinate of point
 	Point(int x, int y, string name) {
@@ -13,24 +13,24 @@ struct Point {
 		this->x = x;
 		this->y = y;
 	}
-    Point(int x, int y){
+    Point(int x, int y) {
         this->x = x;
         this->y = y;
     }
-	Point(){
+	Point() {
 		this->name = "";
 		this->x = -1;
 		this->y = -1;
 	}
-    bool operator<(Point &p) {
-        if(x < p.x)return true;
-        else if(x == p.x){
+    bool operator<(Point& p) {
+        if (x < p.x)return true;
+        else if (x == p.x) {
             return y < p.y;
         }
         else return false;
     }
 };
-class Cell{
+class Cell {
 private:
     string InstName;      // inst name of cell
     string CellName;      // flipflop name of cell
@@ -40,13 +40,15 @@ private:
     vector<Point> Pin;    // pins in cell
     int x;                // x coordinate of cell
     int y;                // y coordinate of cell
-    float slack;          // slack of output point
+	float slack;          // slack of output point
 public:
     Cell();
     Cell(int, int, int);
     void setPos(int, int);
     int getX(void);
+    int getRight(void);
     int getY(void);
+    int getTop(void);
     void setPin(vector<Point>);
     vector<Point> getPin(void);
     int getWidth(void);
