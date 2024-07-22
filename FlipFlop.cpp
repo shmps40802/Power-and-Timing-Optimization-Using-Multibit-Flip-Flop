11e61e54099605b635646f6b201a661a28cebd42
C. Y. Tseng <118676150+shmps40802@users.noreply.github.com>
2024-07-23 01:41:38 +0800

    Update FlipFlop.cpp

--- FlipFlop.cpp
+++ FlipFlop.cpp
@@ -4,15 +4,15 @@ using namespace std;
 FlipFlop::FlipFlop() : Cell(0, 0, 0) {
 	this->N = 0;
 }
-FlipFlop::FlipFlop(int N, int width, int height, int P, vector<Point> pin) 
-: Cell(width, height, P) {
+FlipFlop::FlipFlop(int N, int width, int height, int P, vector<Point> pin)
+	: Cell(width, height, P) {
 	this->N = N;
 	setPin(pin);
 }
 FlipFlop::~FlipFlop() {
-	
+
 }
-int FlipFlop::getN(void){
+int FlipFlop::getN(void) {
 	return N;
 }
 void FlipFlop::setPower(float power) {
@@ -21,7 +21,7 @@ void FlipFlop::setPower(float power) {
 float FlipFlop::getPower(void) {
 	return GatePower;
 }
-void FlipFlop::setQpinDelay(float delay){
+void FlipFlop::setQpinDelay(float delay) {
 	QpinDelay = delay;
 }
 int FlipFlop::getQpinDelay(void) {
@@ -30,22 +30,28 @@ int FlipFlop::getQpinDelay(void) {
 void FlipFlop::display(void) {
 	cout << getInstName() << " " << getX() << " " << getY() << "\n";
 	cout << getCellName() << " " << N << " " << getWidth() << " " << getHeight() << " " << getPinCount() << "\n";
-	for(auto &p : getPin()) {
-		cout << "Pin " <<p .name << " " << p.x << " " << p.y << "\n";
+	for (auto& p : getPin()) {
+		cout << "Pin " << p.name << " " << p.x << " " << p.y << "\n";
 	}
-	for(auto &s : slack) {
+	for (auto& s : slack) {
 		cout << s.first << " " << s.second << "\n";
 	}
 }
-void FlipFlop::setSlack(string PinName, float slack){
-    this->slack[PinName] = slack;
+void FlipFlop::setSlack(string PinName, float slack) {
+	this->slack[PinName] = slack;
 }
 map<string, float> FlipFlop::getSlack(void) {
-    return slack;
+	return slack;
 }
 void FlipFlop::setCluster(int x) {
 	cluster = x;
 }
 int FlipFlop::getCluster(void) {
 	return cluster;
+}
+bool FlipFlop::operator==(const FlipFlop& other) const {
+	if (this->getInstName() == other.getInstName() && this->getCellName() == other.getCellName()) {
+		return true;
+	}
+	return false;
 }
