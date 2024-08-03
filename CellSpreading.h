4ce323421ab3dc366fa2f582f17fd5d270b806a7
C. Y. Tseng <118676150+shmps40802@users.noreply.github.com>
2024-08-03 17:56:45 +0800

    Create CellSpreading.h

---
+++ CellSpreading.h
@@ -0,0 +1,37 @@
+#ifndef CELLSPREADING_H
+#define CELLSPREADING_H
+#include <vector>
+#include <queue>
+#include <iostream>
+#include <climits>
+using namespace std;
+
+struct Edge {
+	int from, to, capacity, residual, cost; //residual is the remaining capacity
+	Edge(int from, int to, int capacity, int cost)
+		: from(from), to(to), capacity(capacity), residual(capacity), cost(cost) {}
+};
+class CellSpreading {
+private:
+	const int MAXN; //maximum number of nodes
+	int s, t, maxFlow, minCost; //source, target, maximum flow, minimum cost
+	vector<Edge> edges;
+	vector<vector<int>> G; //adjacency list, G[i] contains the index of edges that starts from node i
+	vector<int> h; //use potential to make edge cost non-negative
+	vector<int> last; //last edge on shortest path from source to node
+	vector<int> flow; //minimum residual capacity on shortest path from source to node
+	vector<int> cost; //minimum cost from source to node
+	vector<bool> inQueue; //whether the node is in the queue
+	
+
+public:
+	CellSpreading(int, int, int);
+	~CellSpreading();
+	void MCMF(); //Minimum Cost Maximum Flow
+	bool Dijkstra();
+	void addedge(int, int, int, int);
+	void printFlows();
+	int getFlow(int, int);
+};
+#endif
+
