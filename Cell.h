bf7257417ef6b1bc090dfd8054db1e72ef8a5fa6
Perry9124 <124435956+Perry9124@users.noreply.github.com>
2024-08-07 21:11:38 +0800

    Update Cell.h

--- Cell.h
+++ Cell.h
@@ -2,66 +2,70 @@
 #define CELL_H
 #include <vector>
 #include <string>
+#include <set>
+#include <map>
 using namespace std;
+class Cell;
 struct Point {
-    string name;     // name of point
-    char type;       // D, Q, Clk, I, O
-    int x;           // x coordinate of point
-    int y;           // y coordinate of point
-    vector<string> sourcename;
-    Point(int x, int y, string name) {
-        this->name = name;
-        this->x = x;
-        this->y = y;
-    }
-    Point(int x, int y) {
-        this->x = x;
-        this->y = y;
-    }
-    Point() {
-        this->name = "";
-        this->x = -1;
-        this->y = -1;
-    }
-    bool operator<(Point& p) {
-        if (x < p.x)return true;
-        else if (x == p.x) {
-            return y < p.y;
-        }
-        else return false;
-    }
+	string name;     // name of point
+	char type;       // D, Q, Clk, I, O
+	int x;           // x coordinate of point
+	int y;           // y coordinate of point
+	vector<string> sourcename;
+	vector<string> neighbor;
+	Point(int x, int y, string name) {
+		this->name = name;
+		this->x = x;
+		this->y = y;
+	}
+	Point(int x, int y) {
+		this->x = x;
+		this->y = y;
+	}
+	Point() {
+		this->name = "";
+		this->x = -1;
+		this->y = -1;
+	}
+	bool operator<(Point& p) {
+		if (x < p.x)return true;
+		else if (x == p.x) {
+			return y < p.y;
+		}
+		else return false;
+	}
 };
 class Cell {
 private:
-    string InstName;      // inst name of cell
-    string CellName;      // flipflop name of cell
-    int Width;
-    int Height;
-    int PinCounts;
-    vector<Point> Pin;    // pins in cell
-    int x;                // x coordinate of cell
-    int y;                // y coordinate of cell
-    float slack;          // slack of output point
+	int InstNum;        // inst number of cell
+	string CellName;        // flipflop number of cell
+	int Width;
+	int Height;
+	int PinCounts;
+	vector<Point> Pin;  // pins in cell
+	int x;              // x coordinate of cell
+	int y;              // y coordinate of cell
 public:
-    Cell();
-    Cell(int, int, int);
-    void setPos(int, int);
-    int getX(void);
-    int getRight(void);
-    int getY(void);
-    int getTop(void);
-    void setPin(vector<Point>);
-    vector<Point> getPin(void);
-    int getWidth(void);
-    int getHeight(void);
-    int getArea(void);
-    void setInstName(string);
-    string getInstName(void) const;
-    void setCellName(string);
-    string getCellName(void) const;
-    int getPinCount(void);
-    Point getPoint(string);
-    void setsource(string, string);
-    vector<string> getsource(string);
+	Cell();
+	Cell(int, int, int);
+	void setPos(int, int);
+	int getX(void);
+	int getRight(void);
+	int getY(void);
+	int getTop(void);
+	void setPin(vector<Point>);
+	vector<Point> getPin(void);
+	int getWidth(void);
+	int getHeight(void);
+	int getArea(void);
+	void setInstNum(int);
+	int getInstNum(void) const;
+	string getInstName(void) const;
+	void setCellName(string);
+	string getCellName(void) const;
+	int getPinCount(void);
+	Point getPoint(string);
+	void setsource(string, string);
+	vector<string> getsource(string);
 };
 #endif
