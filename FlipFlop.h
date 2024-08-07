be025a912e37065850338b3f7bb7dff5565ca9f7
Perry9124 <124435956+Perry9124@users.noreply.github.com>
2024-08-07 21:12:58 +0800

    Update FlipFlop.h

--- FlipFlop.h
+++ FlipFlop.h
@@ -2,14 +2,16 @@
 #define FLIPFLOP_H
 #include "Cell.h"
 #include <map>
+#include <iostream>
 using namespace std;
 class FlipFlop : public Cell {
 private:
-	int N;                     // n bit(s)
-	float QpinDelay;           // delay time of Qpin
-	float GatePower;           // power of flipflop
-	map<string, float> slack;  // name to slack of flipflop
-	int cluster;               // the cluster it belongs to
+	int N;                       // n bit(s)
+	float QpinDelay;             // delay time of Qpin
+	float GatePower;             // power of flipflop
+	map<string, float> slack;    // name to slack of flipflop
+	int cluster;                 // the cluster it belongs to
+	map<string, map<string, float>> Connection;  // acceptable WL between Q and IN
 public:
 	FlipFlop();
 	FlipFlop(int, int, int, int, vector<Point>);  // N width height P Pin
@@ -18,12 +20,14 @@ public:
 	void setPower(float);
 	float getPower(void);
 	void setQpinDelay(float);
-	int getQpinDelay(void);
+	float getQpinDelay(void);
 	void display(void);
 	void setSlack(string, float);
 	map<string, float> getSlack(void);
 	void setCluster(int);
 	int getCluster(void);
 	bool operator==(const FlipFlop& other) const;
+	void setConnection(string, string, float);
+	map<string, map<string, float>> getConnection(void);
 };
 #endif
