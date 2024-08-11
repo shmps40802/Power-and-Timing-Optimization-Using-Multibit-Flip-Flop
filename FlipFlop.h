2e78db02fdfe93cb3056d569a4f94abf75ec0e45
Perry9124 <124435956+Perry9124@users.noreply.github.com>
2024-08-11 21:27:34 +0800

    Update FlipFlop.h

--- FlipFlop.h
+++ FlipFlop.h
@@ -1,17 +1,19 @@
 #ifndef FLIPFLOP_H
 #define FLIPFLOP_H
 #include "Cell.h"
-#include <map>
+#include <unordered_map>
+#include <unordered_set>
 #include <iostream>
 using namespace std;
 class FlipFlop : public Cell {
 private:
-	int N;                       // n bit(s)
-	float QpinDelay;             // delay time of Qpin
-	float GatePower;             // power of flipflop
-	map<string, float> slack;    // name to slack of flipflop
-	int cluster;                 // the cluster it belongs to
-	map<string, map<string, float>> Connection;  // acceptable WL between Q and IN
+	int N;                                 // n bit(s)
+	float QpinDelay;                       // delay time of Qpin
+	float GatePower;                       // power of flipflop
+	unordered_map<string, float> slack;    // name to slack of flipflop
+	int cluster;                           // the cluster it belongs to
+	unordered_map<string, unordered_map<string, pair<float, string>>> DWL;  // arrival time between D and Q
+	unordered_map<string, unordered_set<string>> Qconnect;                  // arrival time between Q and D
 public:
 	FlipFlop();
 	FlipFlop(int, int, int, int, vector<Point>);  // N width height P Pin
@@ -23,11 +25,13 @@ public:
 	float getQpinDelay(void);
 	void display(void);
 	void setSlack(string, float);
-	map<string, float> getSlack(void);
+	unordered_map<string, float> getSlack(void);
 	void setCluster(int);
 	int getCluster(void);
 	bool operator==(const FlipFlop& other) const;
-	void setConnection(string, string, float);
-	map<string, map<string, float>> getConnection(void);
+	void setDWL(string, string, float, string);
+	unordered_map<string, unordered_map<string, pair<float, string>>> getDWL();
+	void setQconnect(string, string);
+	unordered_map<string, unordered_set<string>> getQconnect(void);
 };
 #endif
