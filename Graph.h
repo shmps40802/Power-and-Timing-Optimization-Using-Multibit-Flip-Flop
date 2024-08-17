b96ba7e6a4fb4dd12700470edce6e6b9d3ae99f4
liz920409 <littlexstar1121@gmail.com>
2024-08-17 22:47:33 +0800

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -11,32 +11,32 @@
 #include "Board.h"
 using namespace std;
 struct Node {
-    int index;
-    string name;
-    Node() : index(-1), name("") {}
-    Node(int index, string name) : index(index), name(name) {}
+	int index;
+	string name;
+	Node() : index(-1), name("") {}
+	Node(int index, string name) : index(index), name(name) {}
 };
 struct Edge {
-    int from;
-    int to;
-    int weight;
-    bool f; // from is Q 
+	int from;
+	int to;
+	int weight;
+	bool f; // from is Q 
 };
 class Graph {
 private:
-    int Num;
-    unordered_map<int, Node> nodes;           // store id and node
-    unordered_map<int, vector<Edge>> adjList; // store id and edge
-    unordered_map<string, int> Index;         // 
+	int Num;
+	unordered_map<int, Node> nodes;           // store id and node
+	unordered_map<int, vector<Edge>> adjList; // store id and edge
+	unordered_map<string, int> Index;         // 
 public:
-    Graph();
-    Graph(Board&);
-    void addNode(const string&);
-    void removeNode(int);
-    void addEdge(int, int, int);
-    void removeEdge(int, int);
-    void setWL(Board&);
-    void dfs(int, int, unordered_map<int, pair<int, int>>&);
+	Graph();
+	Graph(Board&);
+	void addNode(const string&);
+	void removeNode(int);
+	void addEdge(int, int, int);
+	void removeEdge(int, int);
+	void setWL(Board&);
+	void dfs(int index, int WL, unordered_map<int, pair<int, pair<int, int>>>& S, int tmp,unordered_map<int, int>&QWL,int &maxwl);//Q dcon WL Qcon
 };
 
 #endif
