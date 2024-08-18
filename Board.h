620afd397385ad36014ccc2e6d6a24af9c03a5f9
Perry9124 <124435956+Perry9124@users.noreply.github.com>
2024-08-18 21:16:03 +0800

    Update Board.h

--- Board.h
+++ Board.h
@@ -35,11 +35,10 @@ private:
 	friend class Legalization;
 	friend class Graph;
 	friend class G;
-	friend class Placement;
 	float Alpha;
 	float Beta;
 	float Gemma;
-	float Delta;
+	float Lamda;
 	int LowerLeftX;
 	int HigherRightX;
 	int LowerLeftY;
@@ -57,23 +56,22 @@ private:
 	int CellNumber;
 	int minBit;
 	int maxBit;
-	vector<Point> Input;                             // input pin(s)
-	vector<Point> Output;                            // output pin(s)
-	map<string, FlipFlop> FlipFlopLib;               // Cellnumber to FlipFlop
-	map<string, Gate> GateLib;                       // Cellnumber to Gate
-	unordered_map<int, FlipFlop> InstToFlipFlop;     // instnumber to FlipFlop
-	unordered_map<int, Gate> InstToGate;             // instnumber to Gate
-	vector<vector<int>> FlipFlopByClk;               // flipflop(s) with the same clk
-	unordered_map<int, unordered_set<string>> Net;   // netnumer to point name
-	unordered_map<string, int> PointToNet;           // point name to net number C1/D -> (net)1
-	map<int, map<int, vector<int>>> Location;        // location of (inst)number
-	map<int, pair<int, vector<int>>> PlacementRows;  // grid point info
-	set<int> NewFlipFlop;                            // new number (of FlipFlop)
-	map<int, map<int, float>> BinDensity;            // bin density of board
+	vector<Point> Input;                               // input pin(s)
+	vector<Point> Output;                              // output pin(s)
+	map<string, FlipFlop> FlipFlopLib;                 // Cellnumber to FlipFlop
+	map<string, Gate> GateLib;                         // Cellnumber to Gate
+	unordered_map<int, FlipFlop> InstToFlipFlop;       // instnumber to FlipFlop
+	unordered_map<int, Gate> InstToGate;               // instnumber to Gate
+	vector<vector<int>> FlipFlopByClk;                 // flipflop(s) with the same clk
+	unordered_map<int, unordered_set<string>> Net;     // netnumer to point name
+	unordered_map<string, int> PointToNet;             // point name to net number C1/D -> (net)1
+	map<int, map<int, vector<int>>> Location;          // location of (inst)number
+	map<pair<int, int>, vector<int>> PlacementRows;    // grid point info
+	set<int> NewFlipFlop;                              // new number (of FlipFlop)
+	map<int, map<int, float>> BinDensity;              // bin density of board
 	unordered_map<string, unordered_map<string, pair<float, string>>> Ddelay;
 	unordered_map<string, unordered_set<string>> Qconnect;
-	unordered_map<string, pair<string, int>> Dcon;//D  DConect DWL
-	unordered_map<string, unordered_map<string, int>> Qcon;//Q  Qconect QWL
+	unordered_map<string, unordered_set<string>> Net2; 
 public:
 	Board();
 	~Board();
@@ -83,28 +81,31 @@ public:
 	void Plot();
 	Point NametoPoint(string);
 	Cell getCell(int);                                     // get FlipFlop Gate
-	void addNet(int, string);
-	void removeNet(int, string);
-	void Dslack(string, float&, int, int);
-	void Qslack(string, float&, int, int, float);
-	void updateDSlack(string, float&, int, int);
-	void updateQSlack(string, int, int, float);
+	void addNet(int, string);                              // add pin to the net
+	void removeNet(int, string);                           // remove pin from the net
+	void Dslack(string, float&, int, int);                 // compare slack of D pin
+	void Qslack(string, float&, int, int, float);          // compare slack connect to Q pin
+	void updateDSlack(string, float&, int, int);           // update slack of D pin
+	void updateQSlack(string, int, int, float);            // update slack of D pin connected to Q pin
 	void Banking(vector<FlipFlop>, FlipFlop&);             // only banking 1 bit
 	void Debanking(FlipFlop, vector<FlipFlop>&);           // only debanking into 1 bit
-	float bankingCompare(vector<FlipFlop>, FlipFlop);
-	float singleCompare(FlipFlop, FlipFlop);
-	bool Check();
-	int dist(string, string);
+	float bankingCompare(vector<FlipFlop>, FlipFlop);      // compare flipflop with the same bit(s)
+	float singleCompare(FlipFlop, FlipFlop);               // compare flipflop before and after banking
+	bool Check();                                          // check flipflop whether on grid point or overlapping
+	Point getPos(string);
+	int dist(string&, string&);
+	int dist(Point&, Point&);                              // distance between two pins
 	// cost function
 	float TNSCost();
 	float PowerCost();
 	float AreaCost();
-	float BinCost(); // on grid point
+	float BinCost();                                       // on grid point
 	float Cost();
 	int getInstsize();
 	void setDelay(string, string, string, int);
 	void merge(vector<node>&, int, int, int);
 	void mergeSort(vector<node>&, int, int);
-	void setwl(unordered_map<string, pair<string, int>> DCON, unordered_map<string, unordered_map<string, int>> Qcon);
+	void addNet2(string, string);
+	void removeNet2(string, string);
 };
 #endif
