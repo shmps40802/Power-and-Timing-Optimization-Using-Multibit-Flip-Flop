f3b8abd5009f272441a4b2ed9bd32e0cec4137a7
C. Y. Tseng <118676150+shmps40802@users.noreply.github.com>
2024-08-18 21:51:34 +0800

    Update CellSpreading.h

--- CellSpreading.h
+++ CellSpreading.h
@@ -6,16 +6,16 @@
 #include <climits>
 using namespace std;
 
-struct Edge {
+struct Arc {
 	int from, to, capacity, residual, cost; //residual is the remaining capacity
-	Edge(int from, int to, int capacity, int cost)
+	Arc(int from, int to, int capacity, int cost)
 		: from(from), to(to), capacity(capacity), residual(capacity), cost(cost) {}
 };
 class CellSpreading {
 private:
 	const int MAXN; //maximum number of nodes
 	int s, t, maxFlow, minCost; //source, target, maximum flow, minimum cost
-	vector<Edge> edges;
+	vector<Arc> arcs;
 	vector<vector<int>> G; //adjacency list, G[i] contains the index of edges that starts from node i
 	vector<int> h; //use potential to make edge cost non-negative
 	vector<int> last; //last edge on shortest path from source to node
