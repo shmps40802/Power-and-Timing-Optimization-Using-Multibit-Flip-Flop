d1f6a2bc2c2ed561fd6b9d5b065bc0e9b6b06bf1
Perry9124 <124435956+Perry9124@users.noreply.github.com>
2024-08-18 21:09:13 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
+#include <time.h>
 #include "Board.h"
+#include "Algorithm.h"
 #include "Cluster.h"
+#include "Graph.h"
 using namespace std;
 
 int main() {
+	//auto START = clock();
 	Board B;
-	B.ReadFile();
-	B.Display();
+	Graph g(B);
 	Cluster C;
 	C.kmeans(B);
+	//auto END = clock();
+	//cout << (END - START) / CLOCKS_PER_SEC << "s\n";
     return 0;
 }
