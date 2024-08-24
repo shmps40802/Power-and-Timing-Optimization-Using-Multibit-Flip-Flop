8282ad9d75196f8d76e1872c96a77ce0d5ee4e68
Perry9124 <124435956+Perry9124@users.noreply.github.com>
2024-08-24 16:44:28 +0800

    Update Graph.cpp

--- Graph.cpp
+++ Graph.cpp
@@ -73,8 +73,7 @@ void Graph::removeNode(int index) {
 }
 void Graph::addEdge(int from, int to, int weight) {
 	// adjList[from].push_back({from, to, weight, 1, 0});
-	
-	if (nodes[from].name.find("INPUT") != string::npos|| nodes[from].name.find("Q") != string::npos) {
+	if (nodes[from].name.find("INPUT") != string::npos || nodes[from].name.find("Q") != string::npos) {
 		adjList[to].push_back({ to, from, weight, 1 });
 	}
 	else{adjList[to].push_back({ to, from, weight, 0 });}
@@ -92,27 +91,25 @@ void Graph::removeEdge(int from, int to) {
 	}
 }
 void Graph::setWL(Board& B) {
-	ofstream fout;
-	fout.open("connect.txt");
-	unordered_map<int, unordered_map<int, vector<int>>> Dcon;//D Q  DConect WL Qconect QWL
-	unordered_map<string, pair<string, int>> DCON;//D  DConect DWL
-	unordered_map<string, unordered_map<string, int>> Qcon;//Q  Qconect QWL
+	//ofstream fout;
+	//fout.open("connect.txt");
+	unordered_map<int, unordered_map<int, vector<int>>> Dcon;  //D Q DConect WL Qconect QWL
+	unordered_map<string, pair<string, int>> DCON;             //D DConect DWL
+	unordered_map<string, unordered_map<string, int>> Qcon;    //Q Qconect QWL
 	for (auto& f : B.InstToFlipFlop) {
 		for (auto& p : f.second.getPin()) {
 			if (p.type != 'D') continue;
 			int index = Index[f.second.getInstName() + "/" + p.name];
 			unordered_map<string, float>slack = f.second.getSlack();
 			unordered_map<int, pair<int, pair<int, int>>> S; // Q,DConect WL, Qconect
-			unordered_map<int, int>QWL;//Q QWL
+			unordered_map<int, int> QWL;                     //Q QWL
 			int maxwl = 0;
-			unordered_map<int, int>pinwl;
-			dfs(index, 0, S, 0, QWL, maxwl, pinwl);
-			
+			unordered_map<int, int> pinwl;
+			dfs(index, 0, S, -1, QWL, maxwl, pinwl);
 			for (auto& s : S) {
 				s.second.second.first = maxwl;
-
 			}
-			unordered_map<int, vector<int>>SS;
+			unordered_map<int, vector<int>> SS;
 			unordered_map<string, pair<int, string>> Q;
 			for (auto& s : S) {
 				int tt;
@@ -123,7 +120,7 @@ void Graph::setWL(Board& B) {
 						wl = it.weight + slack[p.name] / B.DisplacementDelay;
 					}
 				}
-				vector<int>tmp0({ s.second.first, wl, s.second.second.second, tt });
+				vector<int> tmp0({s.second.first, wl, s.second.second.second, tt });
 				SS[s.first] = tmp0;
 				//fout << nodes[s.first].name<<" "<< nodes[s.second.first].name << " " << nodes[s.second.second.second].name << " "
 				//<< nodes[index].name << " " << s.second.second.first << " "<< tt  <<"\n";
@@ -132,15 +129,12 @@ void Graph::setWL(Board& B) {
 			Dcon[index] = SS;
 		}
 	}
-
 	for (auto& s : Dcon) {
 		for (auto& s1 : s.second) {
 			DCON[nodes[s.first].name] = make_pair(nodes[s1.second[0]].name, s1.second[1]);
-			if (nodes[s1.first].name.find("INPUT")!=string::npos) {
+			if (nodes[s1.first].name.find("INPUT") != string::npos) {
 				continue;
 			}
-		
-				
 			if (Qcon[nodes[s1.first].name].find(nodes[s1.second[2]].name) == Qcon[nodes[s1.first].name].end() || Qcon[nodes[s1.first].name][nodes[s1.second[2]].name] > s1.second[3])
 			{
 				Qcon[nodes[s1.first].name][nodes[s1.second[2]].name] = s1.second[3];
@@ -149,51 +143,39 @@ void Graph::setWL(Board& B) {
 	}
 	//fout << "D: " << endl;
 	for (auto& s : DCON) {
-		fout << s.first << " " << s.second.first << " " << s.second.second << " " << endl;
-
+		//fout << s.first << " " << s.second.first << " " << s.second.second << " " << endl;
 	}
 	//fout << "Q: " << endl;
 	for (auto& s : Qcon) {
 		for (auto& s1 : s.second) {
-			fout << s.first << " " << s1.first << " " << s1.second << " " << endl;
+			//fout << s.first << " " << s1.first << " " << s1.second << " " << endl;
 		}
 	}
 	B.setwl(DCON, Qcon);
 	//fout.close();
 }
-void Graph::dfs(int index, int WL, unordered_map<int, pair<int, pair<int, int>>>& S, int tmp, unordered_map<int, int>&QWL, int &maxwl, unordered_map<int, int>&pinwl) {
+void Graph::dfs(int index, int WL, unordered_map<int, pair<int, pair<int, int>>>& S, int tmp, unordered_map<int, int>& QWL, int& maxwl, unordered_map<int, int>&pinwl) {
 	unordered_map<int, int> dist;
-
 	for (auto& it : adjList[index]) {
+		if (tmp == -1) {
+			tmp = it.to;
+		}
 		if (it.f == 1) {
-			if (tmp == 0) {
-
-				tmp = it.to;
-			}
 			if (S.find(it.to) == S.end() || S[it.to].second.first < WL + it.weight) {
 				if (QWL[it.to] < WL) {
 					QWL[it.to] = WL;
 				}
-				
 				S[it.to] = make_pair(tmp, make_pair(WL + it.weight, it.from));
-				
 				if (WL + it.weight > maxwl) {
 					maxwl = WL + it.weight;
 				}
 			}
 		}
-		else
-		{
-			if (tmp == 0) {
-				
-				tmp = it.to;
-			}	
-			if ( !pinwl[it.to] || pinwl[it.to] < it.weight + pinwl[it.from]) {
+		else {
+			if (pinwl.find(it.to) == pinwl.end() || pinwl[it.to] < it.weight + pinwl[it.from]) {
 				pinwl[it.to] = it.weight + pinwl[it.from];
-				
 				dfs(it.to, WL + it.weight, S, tmp, QWL, maxwl, pinwl);
 		    }
 		}
 	}
-	//cout << "dfs : " << index << endl;
 }
