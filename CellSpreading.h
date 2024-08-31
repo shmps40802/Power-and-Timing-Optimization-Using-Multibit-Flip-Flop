4a79cf8efd45a5a88acfc91b9e972923fb63d946
C. Y. Tseng <118676150+shmps40802@users.noreply.github.com>
2024-08-31 13:26:56 +0800

    Update CellSpreading.h

--- CellSpreading.h
+++ CellSpreading.h
@@ -7,20 +7,22 @@
 using namespace std;
 
 struct Arc {
-	int from, to, capacity, residual, cost; //residual is the remaining capacity
-	Arc(int from, int to, int capacity, int cost)
+	int from, to; //residual is the remaining capacity
+	long long int capacity, cost, residual; //flow from 'from' to 'to'
+	Arc(int from, int to, long long int capacity, long long int cost)
 		: from(from), to(to), capacity(capacity), residual(capacity), cost(cost) {}
 };
 class CellSpreading {
 private:
 	const int MAXN; //maximum number of nodes
-	int s, t, maxFlow, minCost; //source, target, maximum flow, minimum cost
+	int s, t; //source, target, maximum flow, minimum cost
+	long long int maxFlow, minCost;
 	vector<Arc> arcs;
 	vector<vector<int>> G; //adjacency list, G[i] contains the index of edges that starts from node i
-	vector<int> h; //use potential to make edge cost non-negative
+	vector<long long int> h; //use potential to make edge cost non-negative
 	vector<int> last; //last edge on shortest path from source to node
-	vector<int> flow; //minimum residual capacity on shortest path from source to node
-	vector<int> cost; //minimum cost from source to node
+	vector<long long int> flow; //minimum residual capacity on shortest path from source to node
+	vector<long long int> cost; //minimum cost from source to node
 	vector<bool> inQueue; //whether the node is in the queue
 	
 
@@ -29,9 +31,8 @@ public:
 	~CellSpreading();
 	void MCMF(); //Minimum Cost Maximum Flow
 	bool Dijkstra();
-	void addedge(int, int, int, int);
+	void addedge(int, int, long long int, long long int);
 	void printFlows();
-	int getFlow(int, int);
+	long long int getFlow(int, int);
 };
 #endif
-
